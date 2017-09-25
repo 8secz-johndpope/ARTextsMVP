2144670d7e72af2d98de24e710ba4c5c88c1ab82
njligames <jamesfolk1@gmail.com>
2017-09-25 10:16:48 -0400

    first commit

---
+++ ARTexts/ARSession.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  ARTexts
+//
+//  Created by James Folk on 9/25/17.
+//  Copyright © 2017 James Folk. All rights reserved.
+//
+
+import Foundation
---
+++ ARTexts/ARText.swift
@@ -0,0 +1,9 @@
+//
+//  Text.swift
+//  ARTexts
+//
+//  Created by James Folk on 9/25/17.
+//  Copyright © 2017 James Folk. All rights reserved.
+//
+
+import Foundation
